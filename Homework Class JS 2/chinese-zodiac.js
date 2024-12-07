bb851b1063baa9e1465cda9cc4750aa9f424bf53
Kostadinv8 <kostadinvitanov12@gmail.com>
2024-12-07 22:29:23 +0100

    ubdate

--- Homework Class JS 2/chinese-zodiac.js
+++ Homework Class JS 2/chinese-zodiac.js
@@ -1,4 +1,4 @@
-let yearInput = prompt("input your birth year");
+let yearInput = parseInt(prompt("input your birth year"));
 let zodiacs = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig"];
 let calculateZodiac = (yearInput - 4) % 12; //Get zodiac number 
 
