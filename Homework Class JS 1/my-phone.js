ef0ef56fffde83e74da8d166778b9d8d5d9879a9
Kostadinv8 <kostadinvitanov12@gmail.com>
2024-12-07 20:53:27 +0100

    Calculate phone price homework

---
+++ Homework Class JS 1/my-phone.js
@@ -0,0 +1,9 @@
+let phonePrice = 119.95;
+let taxRate = 5;
+let totalPrice = 0;
+
+let quantity = parseInt(prompt("How many phones would you like to purchase:"));
+
+totalPrice = (phonePrice * (1 + taxRate / 100) * quantity).toFixed(2);
+
+console.log("Total Phone Price with tax Rate:", totalPrice);  
