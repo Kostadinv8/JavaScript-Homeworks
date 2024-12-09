d2e5b32eba5d1c4403d23639b5885524cac22549
Kostadinv8 <kostadinvitanov12@gmail.com>
2024-12-09 17:48:07 +0100

    homework update

--- Homework Class JS 2/chinese-zodiac.js
+++ Homework Class JS 2/chinese-zodiac.js
@@ -1,5 +1,44 @@
 let yearInput = parseInt(prompt("input your birth year"));
-let zodiacs = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig"];
-let calculateZodiac = (yearInput - 4) % 12; //Get zodiac number 
+printZodiac(yearInput);
+function printZodiac(year)
+{
+    let calculateZodiac = (year- 4) % 12; //Get zodiac number 
 
-alert(zodiacs[calculateZodiac]);
+    if (calculateZodiac==0) {
+        alert("Your chinese zodiac is Rat");
+    }
+    else if (calculateZodiac==1) {
+        alert("Your chinese zodiac is Ox");
+    }
+    else if (calculateZodiac==2) {
+        alert("Your chinese zodiac is Tiger");
+    }
+    else if (calculateZodiac==3) {
+        alert("Your chinese zodiac is Rabbit");
+    }
+    else if (calculateZodiac==4) {
+        alert("Your chinese zodiac is Dragon");
+    }
+    else if (calculateZodiac==5) {
+        alert("Your chinese zodiac is Snake");
+    }
+    else if (calculateZodiac==6) {
+        alert("Your chinese zodiac is Horse");
+    }
+    else if (calculateZodiac==7) {
+        alert("Your chinese zodiac is Goat");
+    }
+    else if (calculateZodiac==8) {
+        alert("Your chinese zodiac is Monkey");
+    }
+    else if (calculateZodiac==9) {
+        alert("Your chinese zodiac is Rooster");
+    }
+    else if (calculateZodiac==10) {
+        alert("Your chinese zodiac is Dog");
+    }
+    else if (calculateZodiac==11) {
+        alert("Your chinese zodiac is Pig");
+    }
+}
+        
